ea4fb6f034d6b707e68d30b74a8624405e5bba86
adrian1520 <adrian.adamowicz.x@gmail.com>
2026-06-14 16:37:18 +0200

    feat(runtime): add provenance recorder

---
+++ src/runtime/provenance-recorder.ts
@@ -0,0 +1,35 @@
+import { Executor } from "./executor";
+import type { Env } from "../server";
+
+export class ProvenanceRecorder {
+  constructor(
+    private readonly executor = new Executor()
+  ) {}
+
+  async append(
+    env: Env,
+    requestId: string,
+    action: string,
+    payload: unknown
+  ) {
+    return this.executor.runTool(
+      "provenance_append",
+      { action, payload },
+      env,
+      requestId
+    );
+  }
+
+  async query(
+    env: Env,
+    requestId: string,
+    prefix?: string
+  ) {
+    return this.executor.runTool(
+      "provenance_query",
+      { prefix },
+      env,
+      requestId
+    );
+  }
+}
